a632e2d9e0d704ce8572d6488af24f6728232068
Níkolas <nikolas_cardona@hotmail.com>
2020-09-28 15:52:48 -0500

    asdasdasd

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Img1 from '../assets/pexels-jonathan-borba-3279197.jpg'
 
 class NotFound extends Component {
     render() {
         return (
             <div>
-                404 daskljdklasdhasklasdhk
+                404 Lo sentimos :(
+                    Al parecer esta página no existe
+                    <img src={Img1} />
             </div>
         );
     }
