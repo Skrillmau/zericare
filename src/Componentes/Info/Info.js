0fbe33b5c48d6e026bcc909cb4cb9900d6e1d2e8
Skrillmau <mate_balles@hotmail.com>
2020-09-28 17:05:41 -0500

    logout

--- src/Componentes/Info/Info.js
+++ src/Componentes/Info/Info.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-import Paragraph from '../Paragraph/Paragraph'
-import classes from './Info.module.css'
-import Button from '../Button/txt/txtButton'
+import React, { Component } from "react";
+import Paragraph from "../Paragraph/Paragraph";
+import classes from "./Info.module.css";
+import Button from "../Button/txt/txtButton";
 class Info extends Component {
-    render() {
-        return (
-            <div className={classes.block}>
-                <div className={classes.gridContainer}>
-                    <div className={classes.gridItem1}>
-                        <img className={classes.imagen} src={this.props.imagen} />
-                    </div>
+  render() {
+    return (
+      <div className={classes.block}>
+        <div className={classes.gridContainer}>
+          <div className={classes.gridItem1}>
+            <img className={classes.imagen} src={this.props.imagen} />
+          </div>
 
-                    <div className={classes.gridItem2}>
-                        Nombre: <Paragraph contenido={this.props.nombre} clase={"importante"} />
-                        Apellido: <Paragraph contenido={this.props.apellido} clase={"importante"} />
-                        Registro: <Paragraph contenido={this.props.id} clase={"importante"} />
-                        Sexo: <Paragraph contenido={this.props.sexo} clase={"importante"} />
-                        Ocupacion:<Paragraph contenido={this.props.ocupacion} clase={"importante"} />
-                        <Button color="red">Cerrar Sesión</Button>
-                    </div>
-                   
-                </div>
-            </div>
-        );
-    }
+          <div className={classes.gridItem2}>
+            Nombre:
+            <Paragraph contenido={this.props.nombre} clase={"importante"} />
+            Apellido:
+            <Paragraph contenido={this.props.apellido} clase={"importante"} />
+            Registro:
+            <Paragraph contenido={this.props.id} clase={"importante"} />
+            Sexo: <Paragraph contenido={this.props.sexo} clase={"importante"} />
+            Ocupacion:
+            <Paragraph contenido={this.props.ocupacion} clase={"importante"} />
+            <Button onclick={this.props.logout} color="red">Cerrar Sesión</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Info;
--- src/Pages/Perfil.js
+++ src/Pages/Perfil.js
@@ -1,48 +1,69 @@
-import React, { Component } from 'react';
-import Info from '../Componentes/Info/Info'
-import Historia from '../Componentes/Historia/Historia'
-import classes from '../Pages/Perfil/Perfil.css'
+import React, { Component } from "react";
+import Info from "../Componentes/Info/Info";
+import Historia from "../Componentes/Historia/Historia";
+import classes from "../Pages/Perfil/Perfil.css";
 import { withRouter } from "react-router-dom";
-import axios from 'axios'
-import BannerText from '../Componentes/BannerText/BannerText'
+import axios from "axios";
+import BannerText from "../Componentes/BannerText/BannerText";
 class Perfil extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
+    this.state = {
+      usuario: {},
+    };
+  }
 
-        this.state = {
-            usuario: {
+  componentDidMount() {
+    const { id } = this.props.match.params;
 
-            }
-        }
-    }
-
-    componentDidMount() {
-        const { id } = this.props.match.params;
-        
-        console.log(this.props);
-        axios.get(`https://api.npoint.io/05cf5d45abf19bfa8d4f/pacientes/${id}`, {})
-            .then((res) => {
-                const data = res.data;
-                console.log(data);
-                this.setState({ usuario: data });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-    }
-    render() {
-        return (
-            <div className={classes.block}>
-                <h1>Perfil</h1>
-                <Info imagen= {this.state.usuario.imagen} nombre = {this.state.usuario.nombre} apellido = {this.state.usuario.apellido} id={this.state.usuario.registro} sexo={this.state.usuario.sexo} ocupacion={this.state.usuario.ocupacion}/>
-                 <BannerText title="Tu historia clínica">En el listado a continuación, podrás ver tus historial médico más reciente (recuerda darle tu código al médico encargado de tu consulta para que pueda actualizar tu historial)</BannerText>
-                <Historia patologicos={this.state.usuario.patologicos} npatologicos= {this.state.usuario.nopatologicos} familiares= {this.state.usuario.familiares} go={this.state.usuario.gineco} consulta={this.state.usuario.PEEA} dnr={this.state.usuario.dnr} ef={this.state.usuario.fisica}/>
-                
-            </div>
-        );
-    }
+    console.log(this.props);
+    axios
+      .get(`https://api.npoint.io/05cf5d45abf19bfa8d4f/pacientes/${id}`, {})
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+        this.setState({ usuario: data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  handleLogout = () =>{
+      console.log("hola");
+    localStorage.removeItem("user");
+    this.props.history.push(`/`);
+}
+  render() {
+    return (
+      <div className={classes.block}>
+        <h1>Perfil</h1>
+        <Info
+          logout={this.handleLogout}
+          imagen={this.state.usuario.imagen}
+          nombre={this.state.usuario.nombre}
+          apellido={this.state.usuario.apellido}
+          id={this.state.usuario.registro}
+          sexo={this.state.usuario.sexo}
+          ocupacion={this.state.usuario.ocupacion}
+        />
+        <BannerText title="Tu historia clínica">
+          En el listado a continuación, podrás ver tus historial médico más
+          reciente (recuerda darle tu código al médico encargado de tu consulta
+          para que pueda actualizar tu historial)
+        </BannerText>
+        <Historia
+          patologicos={this.state.usuario.patologicos}
+          npatologicos={this.state.usuario.nopatologicos}
+          familiares={this.state.usuario.familiares}
+          go={this.state.usuario.gineco}
+          consulta={this.state.usuario.PEEA}
+          dnr={this.state.usuario.dnr}
+          ef={this.state.usuario.fisica}
+        />
+      </div>
+    );
+  }
 }
 
 export default withRouter(Perfil);
