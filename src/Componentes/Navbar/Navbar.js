aa19c3ce380cf1c296ddbf9aa2507fc32e834cfc
pineapplexpres <luisalejandropena425@gmail.com>
2020-09-27 21:49:45 -0500

    actualizacion

--- src/Componentes/Navbar/Navbar.js
+++ src/Componentes/Navbar/Navbar.js
@@ -1,33 +1,76 @@
 import React, { PureComponent } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import classes from './Navbar.css'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
 
 
 
 class Navbar extends PureComponent {
+
+
     render() {
+        const [anchorEl, setAnchorEl] = React.useState(null);
+
+        const handleClick = (event) => {
+          setAnchorEl(event.currentTarget);
+        };
+      
+        const handleClose = () => {
+          setAnchorEl(null);
+        };
         return (
+
             <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" className={classes.title}>
+                            Zer
+                </Typography>
+                        <Button color="inherit">Sobre Nosotros</Button>
+                        <Button color="inherit">Login</Button>
+                        <Button color="inherit">Login</Button>
+                        <Button color="inherit">Login</Button>
+                        <Button color="inherit">
+                            <AccountCircleIcon></AccountCircleIcon>
+                        </Button>
+
+                        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                            Open Menu
+                        </Button>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </Menu>
+
+                    </Toolbar>
+                </AppBar>
+            </div>
         )
+
     }
+
+    
+
+
+
 }
 
 export default Navbar
+
+
+
+
