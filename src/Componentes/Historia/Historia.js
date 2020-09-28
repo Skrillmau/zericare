a2a26258b0b011f87e148030ea6322d509191fb7
Níkolas <nikolas_cardona@hotmail.com>
2020-09-27 23:47:15 -0500

    hjk

--- src/Componentes/Historia/Historia.js
+++ src/Componentes/Historia/Historia.js
@@ -5,7 +5,13 @@ class Historia extends Component {
     render() {
         return (
             <div>
-                
+              Antecedentes Patológicos: <Paragraph contenido= {this.props.patologicos} clase={"importante"}/>
+              Antecedentes no Patológicos: <Paragraph contenido= {this.props.npatologicos} clase={"importante"}/>
+              Antecedentes familiares: <Paragraph contenido= {this.props.familiares} clase={"importante"}/>
+              Antecedentes Gineco-obstétricos: <Paragraph contenido= {this.props.go} clase={"importante"}/>
+              PEEA última consulta:<Paragraph contenido= {this.props.consulta} clase={"importante"}/>
+              DNR: <Paragraph contenido= {this.props.dnr} clase={"importante"}/>
+              Exploración Física:<Paragraph contenido= {this.props.ef} clase={"importante"}/>
             </div>
         );
     }
