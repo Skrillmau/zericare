f37213b8aecfd1c0f05beea74496c56116091b0d
pineapplexpres <luisalejandropena425@gmail.com>
2020-09-28 05:30:13 -0500

    guenas

--- src/Componentes/ProductInfo/ProductInfo.js
+++ src/Componentes/ProductInfo/ProductInfo.js
@@ -17,7 +17,7 @@ class ProductInfo extends PureComponent {
         //const { id } = this.props.match.params;
         const id = this.props.id;
         console.log(this.props);
-        axios.get(`https://api.npoint.io/091cb40bb6d0f229f0d2/${id}`, {})
+        axios.get(`https://api.npoint.io/091cb40bb6d0f229f0d2/productos/${id}`, {})
             .then((res) => {
                 const data = res.data;
                 console.log(data);
@@ -26,13 +26,70 @@ class ProductInfo extends PureComponent {
             .catch((error) => {
                 console.log(error);
             });
-            
+
     }
     render() {
         return (
             <div className={classes.gridContainer}>
-                <div className={classes.gridItem}>1</div>
-                <div className={classes.gridItem}>2</div>
+                <div className={classes.gridItem}>
+
+                    <img src={this.state.producto.imagen}></img>
+
+                </div>
+
+                <div className={classes.gridItem}>
+                    <div className={classes.sombra}>
+                        <div className={classes.titulo}>
+                            {this.state.producto.nombre}
+                        </div>
+
+                        <br></br>
+
+                        <div className={classes.subtitulo}>
+                            {this.state.producto.presentacion}
+                        </div>
+
+                        <br></br>
+
+                        <div className={classes.categoria}>
+                            {this.state.producto.categoria}
+                        </div>
+                    </div>
+
+
+                    <br></br>
+
+                    <div className={classes.gridrow}>
+
+                        <div className={classes.gridItem3}>
+                            <i class="fas fa-flask"></i>
+                        </div>
+
+                        <div className={classes.gridItem2}>
+                            <p>Formula</p>
+                            {this.state.producto.formula}
+                        </div>
+
+                        <div className={classes.gridItem3}>
+                            <i class="fas fa-book-medical"></i>
+                        </div>
+
+                        <div className={classes.gridItem2}>
+                            <p>Indicaciones</p>
+                            {this.state.producto.indicaciones}
+                        </div>
+
+                        <div className={classes.gridItem3}>
+                            <i class="fas fa-pills"></i>
+                        </div>
+
+                        <div className={classes.gridItem2}>
+                            <p>Vía de administración</p>
+                            {this.state.producto.via}
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
