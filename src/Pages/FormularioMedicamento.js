10112296978c08cfe0129bb2f8c3071ca2e2abaf
Níkolas <nikolas_cardona@hotmail.com>
2020-11-07 21:35:58 -0500

    dsadsasad

---
+++ src/Pages/FormularioMedicamento.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormularioMedicamento = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FormularioMedicamento;
