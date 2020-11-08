454ab7ba2e7c73aaea511ea32501516097aaaaa2
Níkolas <nikolas_cardona@hotmail.com>
2020-11-07 20:25:21 -0500

    asdasd

---
+++ src/Componentes/Pacientes/Pacientes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const pacientes = (props) => {
+    return (
+        <div>
+            <div>
+                <img src="https://www.lavozdemichoacan.com.mx/wp-content/uploads/2020/06/WhatsApp-Image-2020-06-14-at-6.22.29-PM.jpga"/>
+            </div>
+            <div>
+                <p>Nombre:</p>
+                <p>Id:</p>
+                <p>Visto última vez:</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default pacientes;
--- src/Pages/PerfilM.js
+++ src/Pages/PerfilM.js
@@ -5,15 +5,15 @@ import BannerText from "../Componentes/BannerText/BannerText";
 const PerfilM = (props) => {
     return (
         <div>
-               <Info
-          logout={this.handleLogout}
-          imagen={this.state.usuario.imagen}
-          nombre={this.state.usuario.nombre}
-          apellido={this.state.usuario.apellido}
-          id={this.state.usuario.registro}
-          sexo={this.state.usuario.sexo}
-          ocupacion={this.state.usuario.ocupacion}
-        />
+            <Info
+                logout={this.handleLogout}
+                imagen={this.state.usuario.imagen}
+                nombre={this.state.usuario.nombre}
+                apellido={this.state.usuario.apellido}
+                id={this.state.usuario.registro}
+                sexo={this.state.usuario.sexo}
+                ocupacion={this.state.usuario.ocupacion}
+            />
         </div>
     );
 };
