77daaefb02bae0f3678748ed79de06cfae56dc62
Níkolas <nikolas_cardona@hotmail.com>
2020-11-07 23:00:17 -0500

    add todo list medicamentos

--- src/Componentes/OrdenFinal/OrdenFinal.js
+++ src/Componentes/OrdenFinal/OrdenFinal.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const OrdenFinal = (props) => {
+
     return (
         <div>
-            
+            <div>
+    <p>Nombre: {props.nombre}</p>
+    <p>Cantidad: {props.cantidad}</p>
+            </div>
+            <div>
+    <p>Dosis: {props.dosis}</p>
+    <p>Observaciones: {props.observaciones}</p>
+            </div>
         </div>
     );
 };
--- src/Pages/FormularioMedicamento.js
+++ src/Pages/FormularioMedicamento.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Orden from '../Componentes/Orden/Orden'
+import OrdenFinal from '../Componentes/OrdenFinal/OrdenFinal';
+
 
 const FormularioMedicamento = (props) => {
+    const [Items, setItems] = useState([]);
+    const [Selecteditem, setSelecteditem] = useState(null);
+
+    const addMedicamentos = async (e) => {
+        e.preventDefault();
+        console.log(Items);
+        const item = {
+            Nombre: e.target[0].value,
+            Cantidad: e.target[1].value,
+            Dosis: e.target[2].value,
+            Observaciones: e.target[3].value
+        };
+        console.log(item);
+
+        await setItems(Items.concat(item));
+    }
+    const deleteMedicamentos = (index) => {
+        const test = [...Items]
+        test.splice(index, 1)
+        console.log(test, "test")
+        setItems(test);
+    }
     return (
         <div>
-            <p>sapo hijueputaa</p>
+            <p>Agregar Medicamento</p>
+            <Orden onsubmit={addMedicamentos} />
+            <div>
+            {Items.map((item, i) => {
+
+                return (
+
+                    <OrdenFinal nombre={item.Nombre} cantidad={item.Cantidad} dosis={item.Dosis} observaciones={item.Observaciones} deleteItem={() => props.deleteItem(i)} />
+                )
+            })}
+            </div>
         </div>
     );
 };
