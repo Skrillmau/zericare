de04af614309e882dff728384754acae0d960f92
Skrillmau <mate_balles@hotmail.com>
2020-11-08 21:07:46 -0500

    img dropzone

--- src/Componentes/FormularioPaciente/FormularioPaciente.js
+++ src/Componentes/FormularioPaciente/FormularioPaciente.js
@@ -1,62 +1,91 @@
-import { Button } from '@material-ui/core';
-import React, { useMemo } from 'react';
-import { useDropzone } from 'react-dropzone';
-import Buttom from "../Button/txt/txtButton.js";
-import Style from './FormularioPaciente.css';
-
-
-
-const baseStyle = {
-  flex: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  padding: '20px',
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: '#eeeeee',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
-  outline: 'none',
-  transition: 'border .24s ease-in-out'
-};
-
-const activeStyle = {
-  borderColor: '#2196f3'
-};
-
-const acceptStyle = {
-  borderColor: '#00e676'
-};
-
-const rejectStyle = {
-  borderColor: '#ff1744'
-};
+import { StylesProvider } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import Dropzone from "react-dropzone";
+import Button from "../Button/txt/txtButton";
+import Style from "./FormularioPaciente.css";
+import Icon from "@mdi/react";
+import Swal from "sweetalert2";
+import {mdiWindowClose } from "@mdi/js";
 
 
 const FormularioPaciente = (props) => {
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject
-  } = useDropzone({ accept: 'image/*' });
+  const [files, setFiles] = useState([]);
+  const [images, setImages] = useState([]);
+  const [isHovered, setHovered] = useState("");
 
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isDragActive ? activeStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {})
-  }), [
-    isDragActive,
-    isDragReject,
-    isDragAccept
-  ]);
+  const thumbs = files.map((file, i) => (
+    <div
+      className={Style.thumb}
+      key={`thumb-${i}`}
+      onMouseEnter={() => {
+        setHovered(i);
+      }}
+      onMouseLeave={() => {
+        setHovered("");
+      }}
+    >
+      <div className={Style.thumbInner}>
+        <div
+          className={isHovered === i ? Style.deleteImg : Style.hide}
+          onClick={() => deleteImg(i)}
+        >
+          <Icon
+            path={mdiWindowClose}
+            size={1}
+            title="Eliminar imagen"
+            color="#ce0058"
+          />
+        </div>
 
+        <img
+          src={file}
+          className={Style.img}
+          onError={(e) => {
+            e.target.onerror = null;
+            console.log(e);
+          }}
+        />
+      </div>
+    </div>
+  ));
+  const prettier = (data) => {
+		let newImages = data.map((file) => {
+			let preview = URL.createObjectURL(file);
+
+			return { preview };
+		});
+
+		return newImages;
+	};
+  async function deleteImg(index) {
+    setFiles([...files.slice(0, index), ...files.slice(index + 1)]);
+    setImages([...images.slice(0, index), ...images.slice(index + 1)]);
+  }
+  async function onDrop(acceptedFiles) {
+    console.log(acceptedFiles);
+    if (acceptedFiles.length === 0) {
+      return;
+    } else if (acceptedFiles.length > 0 && files.length < 3) {
+      let im = prettier(acceptedFiles);
+      console.log(im);
+      im.map((img) => {
+        setFiles([...files, img.preview]);
+      });
+
+      acceptedFiles.map((file) => {
+        setImages([...images, file]);
+      });
+    } else {
+      Swal.fire({
+        title: "Maximo de imagenes insertadas",
+        text: "por favor elimine una imagen antes de añadir una nueva",
+        icon: "error",
+        confirmButtonColor: "#CE0058",
+      });
+    }
+  }
+  
   return (
-
     <div className={Style.item2}>
       <h3 className={Style.subtitulo}>Agregar Paciente</h3>
       <form className={Style.form} onSubmit="" id="contactForm">
@@ -103,22 +132,22 @@ const FormularioPaciente = (props) => {
             />
             <br />
           </div>
-          <div className={Style.input}>
-            <div className="container">
-              <div {...getRootProps({ style })}>
-                <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
-              </div>
-            </div>
-          </div>
-          <Button color={"blue"} type="submit" >Agregar</Button>
+          <Dropzone onDrop={onDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div {...getRootProps({ className: Style.dropzone })}>
+                  <input {...getInputProps()} />
+                  <p>Agregar imagenes</p>
+                </div>
+                <aside className={Style.thumbsContainer}>{thumbs}</aside>
+              </section>
+            )}
+          </Dropzone>
+          
         </div>
+        <Button color={"blue"} type="submit" >Enviar</Button>
       </form>
     </div>
-
-
-
-
   );
 };
 
