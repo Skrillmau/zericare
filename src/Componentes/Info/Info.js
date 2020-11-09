5e00ab0a75a04970ce5ca3f36b881ec16141a618
pineapplexpres <luisalejandropena425@gmail.com>
2020-11-09 16:25:51 -0500

    botones

--- src/Componentes/Info/Info.js
+++ src/Componentes/Info/Info.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import Paragraph from "../Paragraph/Paragraph";
 import classes from "./Info.module.css";
 import Button from "../Button/txt/txtButton";
+import GroupAddOutlinedIcon from '@material-ui/icons/GroupAddOutlined';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 
 var QRCode = require('qrcode.react');
 class Info extends Component {
   render() {
-    if(this.props.usuario.tipo=="Paciente"){
-    return (
-      <div className={classes.block}>
-        <div className={classes.superflex}>
-          
+    if (this.props.usuario.tipo == "Paciente") {
+      return (
+        <div className={classes.block}>
+          <div className={classes.superflex}>
+
             <div className={classes.gridContainer}>
               <div className={classes.gridItem1}>
                 <img className={classes.imagen} src={this.props.imagen} />
@@ -27,28 +29,34 @@ class Info extends Component {
             <Paragraph contenido={this.props.usuario.ocupacion} clase={"importante"} />
               </div>
             </div>
-          <div className={classes.gridItem3}>
-            <QRCode size="420" value="http://facebook.github.io/react/" />
-       
-          </div>
-         
-        </div>
-        <Button color="blue" onclick={this.props.onclick}>Editar Perfil</Button>
-      </div>
-      
+            <div className={classes.gridItem3}>
+              <QRCode size="420" value="http://facebook.github.io/react/" />
 
-    );}
-    else{
+            </div>
+
+
+
+          </div>
+          
+          <div className={classes.addBoton}>
+          <Button color="blue" onclick={this.props.onclick}><EditOutlinedIcon/></Button>
+          </div>
+        </div>
+
+
+      );
+    }
+    else {
       return (
         <div className={classes.block}>
           <div className={classes.superflex}>
-            
-              <div className={classes.gridContainer}>
-                <div className={classes.gridItem1}>
-                  <img className={classes.imagen} src={this.props.imagen} />
-                </div>
-                <div className={classes.gridItem2}>
-                  Nombre:
+
+            <div className={classes.gridContainer}>
+              <div className={classes.gridItem1}>
+                <img className={classes.imagen} src={this.props.imagen} />
+              </div>
+              <div className={classes.gridItem2}>
+                Nombre:
               <Paragraph contenido={this.props.usuario.nombre} clase={"importante"} />
               Apellido:
               <Paragraph contenido={this.props.usuario.apellido} clase={"importante"} />
@@ -57,14 +65,14 @@ class Info extends Component {
               Sexo: <Paragraph contenido={this.props.usuario.sexo} clase={"importante"} />
               Ocupacion:
               <Paragraph contenido={this.props.usuario.ocupacion} clase={"importante"} />
-                </div>
               </div>
+            </div>
           </div>
         </div>
-  
+
       );
     }
-  
+
   }
 }
 
