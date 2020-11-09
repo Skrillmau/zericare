f4e2ebceb5b0b6f59648a6ee8396e054d633c3c7
pineapplexpres <luisalejandropena425@gmail.com>
2020-11-09 04:30:07 -0500

    nikolashijueputa

--- src/Componentes/Info/Info.js
+++ src/Componentes/Info/Info.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import Paragraph from "../Paragraph/Paragraph";
 import classes from "./Info.module.css";
 import Button from "../Button/txt/txtButton";
+
+var QRCode = require('qrcode.react');
 class Info extends Component {
   render() {
     return (
       <div className={classes.block}>
-        <div className={classes.gridContainer}>
-          <div className={classes.gridItem1}>
-            <img className={classes.imagen} src={this.props.imagen} />
-          </div>
-
-          <div className={classes.gridItem2}>
-            
-            Nombre:
+        <div className={classes.superflex}>
+          
+            <div className={classes.gridContainer}>
+              <div className={classes.gridItem1}>
+                <img className={classes.imagen} src={this.props.imagen} />
+              </div>
+              <div className={classes.gridItem2}>
+                Nombre:
             <Paragraph contenido={this.props.nombre} clase={"importante"} />
             Apellido:
             <Paragraph contenido={this.props.apellido} clase={"importante"} />
@@ -22,9 +24,17 @@ class Info extends Component {
             Sexo: <Paragraph contenido={this.props.sexo} clase={"importante"} />
             Ocupacion:
             <Paragraph contenido={this.props.ocupacion} clase={"importante"} />
+              </div>
+            </div>
+          
+
+
+          <div className={classes.gridItem3}>
+            <QRCode size="420" value="http://facebook.github.io/react/" />
           </div>
         </div>
       </div>
+
     );
   }
 }
