cd4bfd4033680900e5058b4a48c60bef6ec24713
pineapplexpres <luisalejandropena425@gmail.com>
2020-11-09 15:32:54 -0500

    medicamentos

--- src/Componentes/Medicamento/Medicamento.js
+++ src/Componentes/Medicamento/Medicamento.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import classes from './Medicamento.module.css'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 const Medicamento = (props) => {
     return (
         <div>
-            <p>Nombre: {props.medicamento.Nombre}</p>
-            <p>Cantidad: {props.medicamento.Cantidad}</p>
-            <p>Dosis: {props.medicamento.Dosis}</p>
-            <p>Observaciones: {props.medicamento.Observaciones}</p>
+            <div className={classes.item}>
+                <KeyboardArrowRightIcon />
+                <p>Nombre: {props.medicamento.Nombre}</p>
+            </div>
+            <div className={classes.item2}>
+                <p>Cantidad: {props.medicamento.Cantidad}</p>
+                <p>Dosis: {props.medicamento.Dosis}</p>
+                <p>Observaciones: {props.medicamento.Observaciones}</p>
+            </div>
+
         </div>
     );
 };
